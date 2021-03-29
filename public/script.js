4e1e598287b55f0d2738f0378c5471ca4ed161e8
ryanmoy18 <ryan.moy99@gmail.com>
2021-03-29 01:10:01 -0400

    Updates

--- public/script.js
+++ public/script.js
@@ -1,45 +1,24 @@
 async function getData() {
-  const request = await fetch('/api/meals');
-  const data = await request.json();
-  return data;
-}
+  const request = await fetch('/api/dining');
+  const restaurant = await request.json();
+  const arrayDining = restaurant.data;
+  const diningTable = document.querySelector('.table');
+  console.table(diningTable);
 
-async function getRestaurants() {
-  const restaurantData = getData();
-  restaurantData.data.forEach((restaurant) => {
-    const appendItem = document.createElement('div');
-    appendItem.classList.add('tile', 'has-text-centered', 'is-parent', 'is-3');
+  arrayDining.forEach((index) => {
+    const appendItem = document.createElement('tr');
     appendItem.innerHTML = `
-      <article class = "tile is-child box has-background-link-dark ">
-      <span class = "subtitle has-text-light has-text-weight-bold">
-      ${restaurant.hall_name}</span>
-      <br />
-      <span class = "has-text-light">
-      ${restaurant.hall_address.split(',')[0]}
-      </span>
-      <br />
-      <span class = "has-text-light">
-      ${restaurant.hall_address.split(',')[1]}
-      </span>
-    </article>`;
-    targetBox.append(appendItem);
+      <td>${index.hall_id}</td>
+      <td>${index.hall_name}</td>
+      <td>${index.hall_address}</td>
+    `;
+    diningTable.append(appendItem);
   });
 }
 
-async function setData(data) {
-  localStorage.setItem('data', JSON.stringify(data));
-}
 
 async function windowActions() {
-  console.log('window loaded');
   const data = await getData();
-  console.table(data);
-
-  setData(data);
-  const meals = localStorage.getItem('data');
-  const mealsData = JSON.parse(meals);
-  console.log(meals);
-  console.log(mealsData);
 }
 
 window.onload = windowActions;
